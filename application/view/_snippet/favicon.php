3b75294d427e07715068fa1af233e4e3362eca4d
Kempis <kempisguerrero96@gmail.com>
2020-12-18 11:45:21 -0500

    Last commit

---
+++ application/view/_snippet/favicon.php
@@ -0,0 +1,17 @@
+<link rel="apple-touch-icon" sizes="57x57" href="<?php echo Config::get('URL'); ?>favicon/uees.png">
+<link rel="apple-touch-icon" sizes="60x60" href="<?php echo Config::get('URL'); ?>favicon/uees.png">
+<link rel="apple-touch-icon" sizes="72x72" href="<?php echo Config::get('URL'); ?>favicon/uees.png">
+<link rel="apple-touch-icon" sizes="76x76" href="<?php echo Config::get('URL'); ?>favicon/uees.png">
+<link rel="apple-touch-icon" sizes="114x114" href="<?php echo Config::get('URL'); ?>favicon/uees.png">
+<link rel="apple-touch-icon" sizes="120x120" href="<?php echo Config::get('URL'); ?>favicon/uees.png">
+<link rel="apple-touch-icon" sizes="144x144" href="<?php echo Config::get('URL'); ?>favicon/uees.png">
+<link rel="apple-touch-icon" sizes="152x152" href="<?php echo Config::get('URL'); ?>favicon/uees.png">
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo Config::get('URL'); ?>favicon/uees.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="<?php echo Config::get('URL'); ?>favicon/uees.png">
+<link rel="icon" type="image/png" sizes="32x32" href="<?php echo Config::get('URL'); ?>favicon/uees.png">
+<link rel="icon" type="image/png" sizes="96x96" href="<?php echo Config::get('URL'); ?>favicon/uees.png">
+<link rel="icon" type="image/png" sizes="16x16" href="<?php echo Config::get('URL'); ?>favicon/uees.png">
+<link rel="manifest" href="<?php echo Config::get('URL'); ?>favicon/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="<?php echo Config::get('URL'); ?>favicon/uees.png">
+<meta name="theme-color" content="#ffffff">
